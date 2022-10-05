e4e512bbe697d183a47a235b790643a48eb915cd
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-05 17:53:43 +0200

    Car Class and test for price

---
+++ TicketSystemClassLibrary/model/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.model
+{
+    public class Car
+    {
+
+        // Properties
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+        public double price;
+
+        // methods
+        public double Price(double price)
+        {
+            //Price = 240.0;
+            return 240.00;
+        }
+
+        public string VehicleType(string Car)
+        {
+            return Car;
+        }
+
+        public override string ToString()
+        {
+            return $" {nameof(Price)}: {Price} ";
+        }
+    }
+}
---
+++ TicketSystemClassLibraryTests/model/CarTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketSystemClassLibrary.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.model.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        [DataRow(240.0)]
+        public void TestPriceOK(double price)
+        {
+            // Arrange
+            Car c = new Car();
+            double expectedPrice = price;
+
+            // Act
+            c.price = price;
+
+            // Assert
+            Assert.AreEqual(expectedPrice,price);
+
+        }
+
+        [TestMethod()]
+        [DataRow(230.0)]
+        [DataRow(244.0)]
+        public void TestPriceFail(double price)
+        {
+            //Arrange
+            Car c = new Car();
+            double failprice = price;
+
+            //Act
+            c.price = failprice;
+
+            //Assert
+            Assert.AreEqual(failprice,price);
+
+        }
+
+    }
+}
