fd31124137d9f5cdf624f677d1514f39b1b40cb7
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-09 17:11:22 +0200

    Test Storebaeltpris -failling

---
+++ StoreBaeltTicketLibraryTests1/StoreBaeltPriceTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaelt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystemClassLibrary.model;
+
+namespace StoreBaelt.Tests
+{
+    [TestClass()]
+    public class StoreBaeltPriceTests
+    {
+        /// <summary>
+        /// Shared instance field
+        /// </summary>
+        private Car c = null;
+
+        [TestInitialize]
+        public void BeforeEachTestMethod()
+        {
+            c = new Car();
+        }
+
+        [TestMethod()]
+        [DataRow(240.0)] // bil alm
+        [DataRow(125.0)] // MC alm
+        [DataRow(218.75)] // MC med brobizz
+        [DataRow(228.0)] //Bil med brobizz
+        [DataRow(182.4)] // bil med brobizz weekend
+        public void StoreBaeltPriceTestOK(double price)
+        {
+            // Arrange
+            double expectedPrice = price;
+            double delta = 0.001;
+
+            // Act
+            c.Price = price;
+            //double actualprice = c.Price;
+
+            Assert.AreEqual(expectedPrice,price,delta);
+        }
+
+        [TestMethod()]
+
+        public void StoreBaeltPriceTestFail(double price)
+        {
+            Assert.Fail();
+        }
+    }
+}
