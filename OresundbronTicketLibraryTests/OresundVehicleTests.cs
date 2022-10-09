7ed4dcec96e09bfd1edb755f28fc08e99baea266
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-09 19:13:05 +0200

    failing test Oresund

--- OresundbronTicketLibraryTests/OresundVehicleTests.cs
+++ OresundbronTicketLibraryTests/OresundVehicleTests.cs
@@ -5,16 +5,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketSystemClassLibrary.model;
 
 namespace OresundbronTicketLibrary.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class OresundVehicleTests
     {
-        [TestMethod()]
-        public void OresundVehicleTypeTest()
+        // fælles instans
+        OresundVehicle v = null;
+
+        [TestInitialize()]
+        public void BeforeEachTest()
         {
-            Assert.Fail();
+            v = new OresundVehicle();
+        }
+
+        [TestMethod()]
+        [DataRow("OresundCar")]
+        [DataRow("OresundMC")]
+        public void OresundVehicleTypeTestOK(string vehicleType)
+        {
+            // Arrange
+            string expectedVehicleType = vehicleType;
+
+            // Act
+            v.VehicleType = vehicleType;
+
+            //Assert
+            Assert.AreEqual(expectedVehicleType,vehicleType);
         }
     }
 }
