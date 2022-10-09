58f69b61a7335bad52dfadea428e6d5e3a04c531
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-09 14:22:16 +0200

    Storebaelt properties

---
+++ StoreBaelt/StoreBaeltPrice.cs
@@ -0,0 +1,33 @@
+﻿using TicketSystemClassLibrary.model;
+
+namespace StoreBaelt
+{
+    public class StoreBaeltPrice : Vehicle
+    {
+        private double _discount = 1.0;
+        public override bool BroBizz 
+        {
+            get => BroBizz;
+            set => BroBizz = value;
+        }
+        
+        public override string VehicleType 
+        { 
+            get => VehicleType;
+            set => VehicleType = value;
+        }
+        public override double Price
+        {
+            get => Price;
+            set => Price=value;
+        }
+
+        public double Discount
+        {
+            get => _discount;
+            set=> _discount = value;
+        }
+
+
+    }
+}
